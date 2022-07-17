760a1407c9c413b93ecf36e8b8b3131b271bd2aa
LeoKnox <LeoKnox@users.noreply.github.com>
2022-07-16 20:17:33 -0700

    Add files via upload

--- cart/cart.py
+++ cart/cart.py
@@ -7,17 +7,17 @@ class Cart(object):
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
-            cart = self.session[settings.CART_SESSIONS_ID] = {}
+            cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
     def add(self, product, quantity=1, override_quantity=False):
-        productid = str(product.id)
+        product_id = str(product.id)
         if product_id not in self.cart:
             self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
         else:
             self.cart[product_id]['quantity'] += quantity
         self.save()
     def save(self):
-        self.sesion.modified = True
+        self.session.modified = True
     def remove(self, product):
         product_id = str(product.id)
         if product_id in self.cart:
@@ -34,7 +34,7 @@ class Cart(object):
             item['total_price'] = item['price'] * item['quantity']
             yield item
     def __len__(self):
-        return sum(item['quantity'] for item in self.cart.value())
+        return sum(item['quantity'] for item in self.cart.values())
     def get_total_price(self):
         return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
     def clear(self):
